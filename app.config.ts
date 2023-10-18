457591b4e9d2aab4e91f1c6374158010d5c4082e
barbapapazes <e.soubiran25@gmail.com>
2023-10-18 10:25:09 +0200

    chore: lint

--- app.config.ts
+++ app.config.ts
@@ -6,24 +6,24 @@ export default defineAppConfig({
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
       width: 400,
-      height: 300
+      height: 300,
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
-      }
+        alt: 'alpine', // alt of the logo
+      },
     },
     footer: {
       credits: {
         enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine', // our github repository
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Follow me on', // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'nuxtlabs',
@@ -31,11 +31,11 @@ export default defineAppConfig({
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+        href: 'https://www.linkedin.com/company/nuxtlabs',
+      },
     },
     form: {
-      successMessage: 'Message sent. Thank you!'
-    }
-  }
+      successMessage: 'Message sent. Thank you!',
+    },
+  },
 })
